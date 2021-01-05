788455148c9a064946d81a42929a752b328db7a5
drunkestmanevar <fullextentofsuperiority@mail.ru>
2021-01-05 23:54:47 +0300

    Changed API key, cleaned up preset form values

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const FETCH_TIMEOUT = 10;
 export const RESULTS_PER_PAGE = 10;
-export const API_KEY = 'b5f8092f-44ec-4fbc-80dc-2ef7a50593b3';
+export const API_KEY = 'a1ab36bf-1912-4917-9867-a422abe0c48a';
 export const HIDE_WINDOW_SECS = 2.5;
